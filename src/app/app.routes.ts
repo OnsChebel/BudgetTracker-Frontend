e6f93ddfda450627b3c140986a2d65c08c81c011
OnsChebel <cheblnousa@gmail.com>
2026-03-25 11:17:00 +0100

    initial commit with navbar, bootstarp and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import {Accounts} from './components/accounts/accounts';
+import {Budgets} from './components/budgets/budgets';
+import {Transactions} from './components/transactions/transactions';
+import {Categories} from './components/categories/categories';
+import {Dashboard} from './components/dashboard/dashboard';
+import {Login} from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:" ", component: Login
+  },
+  {
+    path: 'accounts', component: Accounts
+  },
+  {
+    path: 'budgets', component: Budgets
+  },
+  {
+    path: 'transactions', component: Transactions
+  },
+  {
+    path:'categories', component: Categories
+  },
+  {
+    path:'dashboard', component: Dashboard
+  }
+];
